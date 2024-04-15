41a3ae80ed1fdb64094136fecc848c944b01fa9f
legolego <oleg_gitgub@olegs.com>
2024-04-15 16:09:31 -0400

    Added more books to search against

--- streamlit/squiggle_search.py
+++ streamlit/squiggle_search.py
@@ -33,12 +33,22 @@ def main():
     with st.sidebar:
         st.markdown("---")
         st.markdown(
+            '<h6>Source in <a href="https://github.com/legolego/GutEmotions"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Github-desktop-logo-symbol.svg/128px-Github-desktop-logo-symbol.svg.png" alt="Github logo" height="40"></a></h6>',
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            '<h6>See in <a href="https://devpost.com/software/project-gutenberg-emotional-analysis"> <img src="http://logo-logos.com/2016/10/Devpost_logo_image_picture.png" alt="Devpost logo" height="20"></a></h6>',
+            unsafe_allow_html=True,
+        )
+        st.markdown(
             '<h6>Made in &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp by Oleg and Travis</h6>',
             unsafe_allow_html=True,
         )
 
+
 def click_button():
     st.session_state.clicked = True
+
 
 def search():
 
@@ -58,10 +68,9 @@ def search():
         )
 
     # Do something interesting with the image data and paths
-    #if canvas_result.image_data is not None:
+    # if canvas_result.image_data is not None:
     #   print("show image")
     #    st.image(canvas_result.image_data)
-
 
     try:
         print("Creating tmp/ directory")
@@ -75,7 +84,7 @@ def search():
     now = time.time()
     N_HOURS_BEFORE_DELETION = .05
     for f in Path("tmp/").glob("*.png"):
-        #st.write(f, os.stat(f).st_mtime, now)
+        # st.write(f, os.stat(f).st_mtime, now)
         if os.stat(f).st_mtime < now - N_HOURS_BEFORE_DELETION * 3600:
             Path.unlink(f)
 
@@ -90,52 +99,53 @@ def search():
         buffered = BytesIO()
         im.save(buffered, format="PNG")
         img_data = buffered.getvalue()
-        try:
-            # some strings <-> bytes conversions necessary here
-            b64 = base64.b64encode(img_data.encode()).decode()
-        except AttributeError:
-            b64 = base64.b64encode(img_data).decode()
+        # try:
+        #     # some strings <-> bytes conversions necessary here
+        #     b64 = base64.b64encode(img_data.encode()).decode()
+        # except AttributeError:
+        #     b64 = base64.b64encode(img_data).decode()
 
-        #dl_link = (
+        # dl_link = (
         #    f'<a download="{file_path}" id="button_id" href="data:file/txt;base64,{b64}">Export PNG</a><br></br>'
-        #)
-        #st.markdown(dl_link, unsafe_allow_html=True)
+        # )
+        # st.markdown(dl_link, unsafe_allow_html=True)
 
         if st.button('Search', on_click=click_button):
             arr_to_plot = gf.get_gemini_img_approx(file_path)
             plt = get_plot_from_squiggle(arr_to_plot)
-            
+
             st.markdown("Gemini Representation of Squiggle:")
             st.pyplot(plt)
 
-            #print(arr_to_plot)
-
+            # print(arr_to_plot)
 
             # Make a random list of book to query against
             random_list = np.random.uniform(-1, 1, size=(10, 20)).tolist()
             rand_titles = ["Random Arc 1", "Random Arc 2", "Random Arc 3", "Random Arc 4", "Random Arc 5", "Random Arc 6", "Random Arc 7", "Random Arc 8", "Random Arc 9", "Random Arc 10"]
             titles = pd.Series(rand_titles)
-            scores= pd.Series(random_list)
+            scores = pd.Series(random_list)
             dfBooks = pd.concat([titles, scores], axis=1)
             dfBooks.columns = ['Title', 'Scores']
 
             dfBooks = pd.read_csv("streamlit/data/books_final2.csv").iloc[:, 1:]
             dfBooks = pd.read_parquet("streamlit/data/gutenberg_books.parquet").iloc[:, 1:]
 
-            #st.dataframe(dfBooks)
+            # st.dataframe(dfBooks)
 
             #######################
 
             target = np.array(arr_to_plot)
             all_dists = []
             for index, row in dfBooks[dfBooks['emotion_scores'].str.len() > 0].iterrows():
-                bookArc = np.array(ast.literal_eval(row['emotion_scores']), dtype=float)
+                bookArc = np.array(ast.literal_eval(row['emotion_scores']),
+                                   dtype=float)
                 print("Arc:", bookArc)
                 distance = dtw.distance_fast(target, bookArc)
                 all_dists.append((row['title'], distance, bookArc))
 
-            sorted_by_second = sorted(all_dists, key=lambda tup: tup[1], reverse=False)
-            #sorted_by_second
+            sorted_by_second = sorted(all_dists, key=lambda tup: tup[1],
+                                      reverse=False)
+            # sorted_by_second
 
             # Create an empty list to store the HTML table rows
             table_rows = []
@@ -160,7 +170,7 @@ def search():
 
                 # Convert the plot to an image
                 buf = io.BytesIO()
-                #plt.figure(figsize=(1,1))
+                # plt.figure(figsize=(1,1))
                 plt.savefig(buf, format='png')
                 buf.seek(0)
                 plot_img = base64.b64encode(buf.getvalue()).decode('utf-8')
@@ -184,21 +194,22 @@ def get_plot_from_squiggle(arr_to_plot, title="Drawn Line"):
     plt.clf()
     # data to be plotted
 
-    #x = arr_to_plot[:,0]
+    # x = arr_to_plot[:,0]
     y = arr_to_plot#[:,1]
 
     # plotting
-    
-    #plt.xlabel("X axis")
-    #plt.ylabel("Y axis")
+
+    # plt.xlabel("X axis")
+    # plt.ylabel("Y axis")
     plt.figure(figsize=(2,.2))
     plt.axis('off')
-    #plt.title(title, fontsize = 3)
+    # plt.title(title, fontsize = 3)
     plt.plot(y, color ="red")
-    
-    
-    #plt.ylim(-1, 1)
+
+
+    # plt.ylim(-1, 1)
     return plt
+
 
 def about():
 
@@ -207,10 +218,11 @@ def about():
     """
     )
 
+
 if __name__ == "__main__":
-    #st.set_page_config(
+    # st.set_page_config(
     #    page_title="Squiggle Search!", page_icon=":pencil2:"
-    #)
+    # )
     st.title("Squiggle Search!")
     st.sidebar.subheader("Navigation")
     main()
